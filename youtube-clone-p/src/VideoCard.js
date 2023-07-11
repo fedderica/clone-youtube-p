1d5664ac45eb4535eccf2079e641f196f37d7066
fedderica <fedderica.bartoli@gmail.com>
2023-07-11 12:03:41 -0300

    videos

--- youtube-clone-p/src/VideoCard.js
+++ youtube-clone-p/src/VideoCard.js
@@ -1,11 +1,27 @@
+import { Avatar } from '@mui/material'
 import React from 'react'
+import './VideoCard.css';
 
-function VideoCard({image, title, channel, }) {
+function VideoCard({image, title, channel, views, timestamp, channelImage }){
   return (
-    <div>
-      <h4> Video </h4>
-    </div>
-  )
+    <div className='videoCard'>
+        <img src={image} alt="" />
+        <div className="videoCard__info">
+            <Avatar
+            className="videoCard__avatar"
+            alt={channel}
+            src={channelImage}
+             />
+             <div className='video__text'>
+                <h4>{title}</h4>
+                <p>{channel}</p>
+                <p>
+                <p>{views} * {timestamp}</p>
+                </p>
+                </div>
+                </div>
+                </div>
+  );
 }
 
 export default  VideoCard
